e67060d5c0895f5e2ed7b3be73e6812f1e7b49d5
W4LLI <learning1830@outlook.com>
2022-04-17 12:43:12 -0500

    figuras geometricas

--- Proyectos/Detección de Figuras Geométricas/figurasGeometricas.py
+++ Proyectos/Detección de Figuras Geométricas/figurasGeometricas.py
@@ -1,11 +1,14 @@
-import cv2
+from cv2 import cv2
+from skimage import io
 
-image = cv2.imread('figurasColores2.png')
+
+# image = cv2.imread(r"D:\Algo\Win\IA\Proyectos\Detección de Figuras Geométricas\figurasColores.jpg")
+image = io.imread(r"D:\Algo\Win\IA\Proyectos\Detección de Figuras Geométricas\figurasColores.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 canny = cv2.Canny(gray, 10, 150)
 canny = cv2.dilate(canny, None, iterations=1)
 canny = cv2.erode(canny, None, iterations=1)
-#_, th = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+# _, th = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
 #_,cnts,_ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)# OpenCV 3
 cnts,_ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)# OpenCV 4
 #cv2.drawContours(image, cnts, -1, (0,255,0), 2)
